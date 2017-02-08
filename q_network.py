03a13a84e0de93fe7286c831a443077a69dd3de1
Albert Shaw <ashaw596@gmail.com>
2017-02-08 14:58:42 -0500

    Fix penalty_coeff stuff actually

--- q_network.py
+++ q_network.py
@@ -263,14 +263,14 @@ class QNetwork():
 			if error_clip >= 0:
 				quadratic_part = tf.clip_by_value(minConstraintDiff, 0.0, error_clip)
 				linear_part = minConstraintDiff - quadratic_part
-				minConstraintError = (penalty_coeff * tf.square(quadratic_part)) + (error_clip * linear_part)
+				minConstraintError = penalty_coeff * (0.5 * tf.square(quadratic_part) + (error_clip * linear_part))
 			else:
-				minConstraintError = (penalty_coeff * tf.square(minConstraintDiff))
+				minConstraintError = penalty_coeff * (0.5 * tf.square(minConstraintDiff))
 
 			if error_clip >= 0:
 				quadratic_part = tf.clip_by_value(maxConstraintDiff, 0.0, error_clip)
 				linear_part = maxConstraintDiff - quadratic_part
-				maxConstraintError = penalty_coeff * (0.5 * tf.square(quadratic_part)) + (error_clip * linear_part)
+				maxConstraintError = penalty_coeff * (0.5 * tf.square(quadratic_part) + (error_clip * linear_part))
 			else:
 				maxConstraintError = penalty_coeff * (0.5 * tf.square(maxConstraintDiff))
 
